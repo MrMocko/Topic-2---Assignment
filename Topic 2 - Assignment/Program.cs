8f415c39cf3199538b473e02d2ed9ae4926979d2
Andre Mocko <339723967@GoTVDSB.ca>
2024-09-10 09:38:58 -0400

    Add project files.

---
+++ Topic 2 - Assignment/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Topic_2___Assignment
+{
+    internal class Program
+    { //Andre 
+        static void Main(string[] args)
+        {
+            Console.WriteLine(2024 - 16);
+            Console.WriteLine((14 + 15 + 16)/ 3);
+            Console.WriteLine(((14.0 + 15 + 16 + 17) / 4));
+            Console.WriteLine(Math.Round(Math.PI *  ((3 * 3)) ,1) + "Cm^2");
+            Console.WriteLine("C# Knows when to do math rather then sticking thigs together by where you put it in your string, for example if you put it outside of your parenthasies it will stick the numbers together and if you dont put any parenthasies then it will add your numbers together");
+            Console.ReadLine("");
+            
+        }
+    }
+}
